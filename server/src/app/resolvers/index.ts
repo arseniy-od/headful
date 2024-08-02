7b8f295f44601e6f714c831cbdafb0e330e102bd
Arseniy Odaisky <odarseniy@gmail.com>
2024-08-02 16:41:47 +0300

    Refactored AIInput to reduce amount of repeating code, other minor improvements

--- server/src/app/resolvers/index.ts
+++ server/src/app/resolvers/index.ts
@@ -12,7 +12,13 @@ const resolvers: any[] = [
             },
             // eslint-disable-next-line
             rephrase: async (_: any, {text}: {text: string}) => {
-                const prompt = `Rephrase content to make it sound more professional: ${text}`;
+                const prompt = `Rephrase content to make it sound more professional. Answer in initial language: ${text}`;
+                const chatGptResponse = await getChatGptResponse(prompt);
+                return chatGptResponse;
+            },
+            // eslint-disable-next-line
+            summarize: async (_: any, {text}: {text: string}) => {
+                const prompt = `Summarize text below. Answer in initial language: ${text}`;
                 const chatGptResponse = await getChatGptResponse(prompt);
                 return chatGptResponse;
             },
